4c5b16129e1ce4f9d9117f4c850ef9a66f6e41b4
Derek <derekmurphy1993@gmail.com>
2025-01-23 14:08:35 -0600

    [style] padding

--- src/pages/m_portfolio.jsx
+++ src/pages/m_portfolio.jsx
@@ -7,7 +7,7 @@ export default function MPortfolio() {
 	return (
 		<div className="flex flex-col max-w-screen-xl mx-auto gap-14 my-12 pb-36">
 			<div
-				className="flex flex-row gap-2 items-center grayscale hover:grayscale-0  text-gray-100 hover:text-[#d3f86b]/80"
+				className="flex flex-row gap-2 ml-2 items-center grayscale hover:grayscale-0  text-gray-100 hover:text-[#d3f86b]/80"
 				onClick={() => window.open("https://mern-rapidrentals.onrender.com/")}
 			>
 				<div className="text-left">
@@ -34,7 +34,7 @@ export default function MPortfolio() {
 			</div>
 
 			<div
-				className="flex flex-row gap-2 items-center grayscale hover:grayscale-0  text-gray-100 hover:text-[#d3f86b]/80"
+				className="flex flex-row gap-2 ml-2 items-center grayscale hover:grayscale-0  text-gray-100 hover:text-[#d3f86b]/80"
 				onClick={() =>
 					window.open(
 						"https://medium.com/@derekmurphy1993/civic-app-retrospective-75321a9ebd7c"
@@ -43,7 +43,7 @@ export default function MPortfolio() {
 			>
 				{" "}
 				<div className="text-left">
-					<h1 className="font-display  font-bold text-4xl">CIVIC APP</h1>
+					<h1 className="font-display font-bold text-4xl">CIVIC APP</h1>
 					<p className="font-display text-xl">Software Dev, React + Google Maps API</p>
 				</div>
 				<div className="w-8/12 h-52 overflow-hidden p-5">
