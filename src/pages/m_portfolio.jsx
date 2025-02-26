2414c2fd1a030199efad4cda982e746a8cba8a5e
Derek <derekmurphy1993@gmail.com>
2025-02-26 12:14:48 -0600

    [fix] link fix

--- src/pages/m_portfolio.jsx
+++ src/pages/m_portfolio.jsx
@@ -1,26 +1,12 @@
 // import "./index.css"
 import SE from "../assets/port/SEModernImage.jpg";
 import RR from "../assets/port/RapidRentalModernImage.jpg";
-import CIV from "../assets/port/CivicModernImage.jpg";
+import CIV from "../assets/port/Civic.jpg";
 import MBTA from "../assets/port/train.png";
 
 export default function MPortfolio() {
 	return (
 		<div className="flex flex-col max-w-screen-xl mx-auto gap-14 my-12 pb-36">
-			{/* MBTA port */}
-
-			<div
-				className="flex flex-row gap-2 ml-2 items-center grayscale hover:grayscale-0  text-gray-100 hover:text-[#d3f86b]/80"
-				onClick={() => window.open("https://mbta-stopwatchh.onrender.com/")}
-			>
-				<div className="text-left">
-					<h1 className="font-display  font-bold text-4xl">MBTA Stopwatch</h1>
-					<p className="font-display text-xl">Realtime Transit App, React</p>
-				</div>
-				<div className="w-8/12 h-52 overflow-hidden p-5 ">
-					<img className="" src={MBTA} />
-				</div>
-			</div>
 			{/* SE port */}
 			<div
 				className="flex flex-row gap-2 items-center grayscale hover:grayscale-0  text-gray-100 hover:text-[#d3f86b]/80"
@@ -31,24 +17,62 @@ export default function MPortfolio() {
 					<img className="" src={SE} />
 				</div>
 				<div className="text-left">
-					<h1 className="font-display   font-bold text-4xl">SCHNIDER ELECTRIC</h1>
-					<p className="font-display text-xl">Front End Developer, React + JS</p>
+					<h1 className="font-display font-bold text-4xl">Schnider Electric</h1>
+					<p className="font-display text-xl">FE Software Engineer, LayoutFast</p>
+					<p className="font-display text-xl">
+						<span className="hover:text-[#e388c5] hover:underline">
+							{" "}
+							LayoutFast Site{" "}
+						</span>
+						/{" "}
+						<span
+							className="font-display text-xl  hover:text-[#e388c5] hover:underline"
+							onClick={() =>
+								window.open(
+									"https://medium.com/@derekmurphy1993/schneider-electric-retrospective-b04124efb5e5"
+								)
+							}
+						>
+							{" "}
+							Blog Post
+						</span>
+					</p>
 				</div>
 			</div>
-			{/* rapid rentals port */}
+
+			{/* MBTA port */}
 
 			<div
 				className="flex flex-row gap-2 ml-2 items-center grayscale hover:grayscale-0  text-gray-100 hover:text-[#d3f86b]/80"
-				onClick={() => window.open("https://mern-rapidrentals.onrender.com/")}
+				onClick={() => window.open("https://mbta-stopwatchh.onrender.com/")}
 			>
 				<div className="text-left">
-					<h1 className="font-display  font-bold text-4xl">RAPID RENTALS</h1>
-					<p className="font-display text-xl">Software Dev, MERN</p>
+					<h1 className="font-display font-bold text-4xl">MBTA StopWatch</h1>
+					<p className="font-display text-xl">Realtime Transit App</p>
+					<p className="font-display text-xl">
+						<span className="hover:text-[#e388c5] hover:underline">
+							{" "}
+							Deployed Site{" "}
+						</span>
+						{/* /{" "}
+						<span
+							className="font-display text-xl  hover:text-[#e388c5] hover:underline"
+							onClick={() =>
+								window.open(
+									"https://medium.com/@derekmurphy1993/civic-app-retrospective-75321a9ebd7c"
+								)
+							}
+						>
+							{" "}
+							Blog Post
+						</span> */}
+					</p>
 				</div>
 				<div className="w-8/12 h-52 overflow-hidden p-5 ">
-					<img className="" src={RR} />
+					<img className="" src={MBTA} />
 				</div>
 			</div>
+
 			{/* Civic port */}
 
 			<div
@@ -64,8 +88,59 @@ export default function MPortfolio() {
 					<img className="" src={CIV} />
 				</div>
 				<div className="text-left">
-					<h1 className="font-display font-bold text-4xl">CIVIC APP</h1>
-					<p className="font-display text-xl">Software Dev, React + Google Maps API</p>
+					<h1 className="font-display font-bold text-4xl">Civic App</h1>
+					<p className="font-display text-xl">Software Engineer </p>
+					<p className="font-display text-xl">
+						{/* <span className="hover:text-[#e388c5] hover:underline">
+							{" "}
+							LayoutFast Site{" "}
+						</span>
+						/{" "} */}
+						<span
+							className="font-display text-xl  hover:text-[#e388c5] hover:underline"
+							onClick={() =>
+								window.open(
+									"https://medium.com/@derekmurphy1993/civic-app-retrospective-75321a9ebd7c"
+								)
+							}
+						>
+							{" "}
+							Blog Post
+						</span>
+					</p>
+				</div>
+			</div>
+
+			{/* rapid rentals port */}
+
+			<div
+				className="flex flex-row gap-2 ml-2 items-center grayscale hover:grayscale-0  text-gray-100 hover:text-[#d3f86b]/80"
+				onClick={() => window.open("https://mern-rapidrentals.onrender.com/")}
+			>
+				<div className="text-left">
+					<h1 className="font-display font-bold text-4xl">RAPID RENTALS</h1>
+					<p className="font-display text-xl">MERN Rental App</p>
+					<p className="font-display text-xl">
+						<span className="hover:text-[#e388c5] hover:underline">
+							{" "}
+							Deployed Site{" "}
+						</span>
+						{/* /{" "}
+						<span
+							className="font-display text-xl  hover:text-[#e388c5] hover:underline"
+							onClick={() =>
+								window.open(
+									"https://medium.com/@derekmurphy1993/civic-app-retrospective-75321a9ebd7c"
+								)
+							}
+						>
+							{" "}
+							Blog Post
+						</span> */}
+					</p>
+				</div>
+				<div className="w-8/12 h-52 overflow-hidden p-5 ">
+					<img className="" src={RR} />
 				</div>
 			</div>
 		</div>
